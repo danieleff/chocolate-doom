8f5416fbf34ec313155dda1997ae5e9b080adb68
Daniel Fekete <danieleff@gmail.com>
2017-05-18 21:50:21 +0200

    arduino: net: no net

--- arduino/net_query.c
+++ arduino/net_query.c
@@ -545,8 +545,10 @@ void NET_Query_Init(void)
     if (query_context == NULL)
     {
         query_context = NET_NewContext();
+#ifndef ARDUINO
         NET_AddModule(query_context, &net_sdl_module);
         net_sdl_module.InitClient();
+#endif
     }
 
     free(targets);
